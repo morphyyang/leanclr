4b4ab6427d24ada518c018f0875a3c18313cb8dd
walon <walon@code-philosophy.com>
2026-05-16 13:43:33 +0800

    fix(runtime): SafeHandle::new_safe_handle  invokes default constructor after creating handle object

--- src/runtime/vmutils/safehandle.cpp
+++ src/runtime/vmutils/safehandle.cpp
@@ -1,5 +1,7 @@
 #include "safehandle.h"
 #include "vm/object.h"
+#include "vm/class.h"
+#include "vm/runtime.h"
 
 namespace leanclr
 {
@@ -25,7 +27,12 @@ void SafeHandle::set_handle(vm::RtObject* o, intptr_t handle) noexcept
 RtResult<vm::RtObject*> SafeHandle::new_safe_handle(const metadata::RtClass* klass, intptr_t handle) noexcept
 {
     DECLARING_AND_UNWRAP_OR_RET_ERR_ON_FAIL(vm::RtObject*, obj, vm::Object::new_object(klass));
-    reinterpret_cast<SafeHandleObject*>(obj)->handle = handle;
+    const metadata::RtMethodInfo* default_ctor = vm::Class::get_method_for_name(klass, ".ctor", 0, false);
+    assert(default_ctor);
+    interp::RtStackObject args[1];
+    args[0].obj = obj;
+    RET_ERR_ON_FAIL(vm::Runtime::invoke_stackobject_arguments_without_run_cctor(default_ctor, args, nullptr));
+    set_handle(obj, handle);
     RET_OK(obj);
 }
 } // namespace vmutils
